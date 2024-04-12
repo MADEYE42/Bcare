80edd5086c8c547275eaf90f965b82789b8559cf
Gouresh  Madye <gmadye13@gmail.com>
2024-04-13 01:35:31 +0530

    Done

--- Main_page/code/script.js
+++
@@ -1,5 +0,0 @@
-var typed = new Typed('#element', {
-    strings: [ 'Graphic Designer','Game Developer','Front-End Developer',
-    'Back-End Developer','Full Stack Developer','Android Developer'],
-    typeSpeed: 50,
-  });
